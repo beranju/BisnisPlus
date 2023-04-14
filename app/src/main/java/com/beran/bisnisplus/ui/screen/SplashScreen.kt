99f6e792427415210adfc1cd88e2945b83981155
beranju <beranjusihombing1@gmail.com>
2023-04-14 12:58:22 +0700

    create splashscreen ui

---
+++ app/src/main/java/com/beran/bisnisplus/ui/screen/SplashScreen.kt
@@ -0,0 +1,49 @@
+package com.beran.bisnisplus.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.beran.bisnisplus.R
+import com.beran.bisnisplus.ui.theme.BisnisPlusTheme
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            Spacer(Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = "App logo"
+            )
+            Text(
+                text = "Catatan keuangan\nusaha kamu",
+                textAlign = TextAlign.Center
+            )
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPrev() {
+    BisnisPlusTheme {
+        SplashScreen()
+    }
+}
