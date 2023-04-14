78fcf4a7de73c9e2857ad3a82c13d6e4b0b8bf14
beranju <beranjusihombing1@gmail.com>
2023-04-14 23:30:41 +0700

    add home screen

---
+++ app/src/main/java/com/beran/bisnisplus/ui/screen/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.beran.bisnisplus.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Box {
+        Text(text = "Hello")
+    }
+}
