b6a2a3536826fdd85473f4f5006c1d8ad75f344d
beranju <beranjusihombing1@gmail.com>
2023-04-14 13:27:38 +0700

    add the google font and implement to splashscreen

--- app/src/main/java/com/beran/bisnisplus/ui/screen/SplashScreen.kt
+++ app/src/main/java/com/beran/bisnisplus/ui/screen/SplashScreen.kt
@@ -3,16 +3,20 @@ package com.beran.bisnisplus.ui.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.beran.bisnisplus.R
 import com.beran.bisnisplus.ui.theme.BisnisPlusTheme
+import com.beran.bisnisplus.ui.theme.fontFamily
 
 @Composable
 fun SplashScreen() {
@@ -32,7 +36,8 @@ fun SplashScreen() {
             )
             Text(
                 text = "Catatan keuangan\nusaha kamu",
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
             )
         }
 
