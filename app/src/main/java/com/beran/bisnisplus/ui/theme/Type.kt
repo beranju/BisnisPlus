312a1127c050e79c625f437cbbdfb0e7d712a0d2
beranju <beranjusihombing1@gmail.com>
2023-04-17 17:20:19 +0700

    add title large to typography

--- app/src/main/java/com/beran/bisnisplus/ui/theme/Type.kt
+++ app/src/main/java/com/beran/bisnisplus/ui/theme/Type.kt
@@ -39,16 +39,18 @@ val Typography = Typography(
         fontSize = 14.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
 
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+
+    /* Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
