1663cc2faab479de6bc45d55eeedcd1080fbae1d
beranju <beranjusihombing1@gmail.com>
2023-04-27 12:20:05 +0700

    slice setting screen

--- app/src/main/java/com/beran/bisnisplus/ui/screen/SettingScreen.kt
+++ app/src/main/java/com/beran/bisnisplus/ui/screen/SettingScreen.kt
@@ -1,26 +1,115 @@
 package com.beran.bisnisplus.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.NavigateNext
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.beran.bisnisplus.R
 import com.beran.bisnisplus.ui.theme.BisnisPlusTheme
 
 @Composable
 fun SettingScreen(
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier
-        .fillMaxSize()
-        .padding(horizontal = 24.dp))
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 16.dp)
+    )
     {
-        Text(text = "SettingScreen")
+        ProfileCard()
+        Spacer(modifier = Modifier.height(50.dp))
+        OptionMenu()
     }
 }
+
+@Composable
+private fun OptionMenu(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.fillMaxWidth()
+    ) {
+        MenuItem(title = "Setting", onClick = { })
+        MenuItem(title = "Log Out", onClick = { })
+    }
+}
+
+@Composable
+private fun MenuItem(
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.fillMaxWidth()) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.weight(1f)
+        )
+        Icon(
+            imageVector = Icons.Outlined.NavigateNext,
+            contentDescription = "action of menu",
+            modifier = Modifier.clickable {
+                onClick()
+            })
+    }
+}
+
+@Composable
+private fun ProfileCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .padding(8.dp)
+    ) {
+        Image(
+            painter = painterResource(id = (R.drawable.img_empty_profile)),
+            contentDescription = "photo profile",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Beranju", style = MaterialTheme.typography.titleSmall)
+            Text(
+                text = "Pedagang Cabai",
+                style = MaterialTheme.typography.bodySmall.copy(fontSize = 10.sp)
+            )
+            Button(onClick = {}, modifier = Modifier.align(Alignment.End)) {
+                Text(text = "Edit")
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
